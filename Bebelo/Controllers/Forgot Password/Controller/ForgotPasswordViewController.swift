9e778b3b212d8f73cfe618e31067287b56ee1981
WaqasMehmood100 <malikwaqas100@hotmail.com>
2021-07-09 18:04:10 +0500

    Forgot Password..

---
+++ Bebelo/Controllers/Forgot Password/Controller/ForgotPasswordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotPasswordViewController.swift
+//  Bebelo
+//
+//  Created by Buzzware Tech on 09/07/2021.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- Bebelo/Controllers/Login/Controller/LoginViewController.swift
+++ Bebelo/Controllers/Login/Controller/LoginViewController.swift
@@ -14,11 +14,31 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
     //IBACTION'S
     @IBAction func LoginButtonAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func AddItHereBtnAction(_ sender: Any) {
+        let vc = self.getViewController(identifier: "AddYourBarViewController") as! AddYourBarViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func ForgotPasswordBtnAction(_ sender: Any) {
+        let vc = self.getViewController(identifier: "ForgotPasswordViewController") as! ForgotPasswordViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+
+//MARK:- HELPING METHOD'S
+extension LoginViewController{
+    func getViewController(identifier:String)-> UIViewController {
+        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(identifier: identifier)
+        return vc
     }
 }
